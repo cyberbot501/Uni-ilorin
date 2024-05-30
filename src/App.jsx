bb5d4f466bd2102cec5b6929767245a3722e900c
cyberbot501 <softwareengineerjoycy@gmail.com>
2024-05-30 09:37:56 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-rou
 import INTRO from  './pages/intro'
 import LOGIN from './pages/login'
 import SIGNUP from './pages/signup'
+import LANDING from './pages/landingPage'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<INTRO />} />,
       <Route path="/login" element={<LOGIN />} />,
       <Route path="/signup" element={<SIGNUP />} />,
+      <Route path="/landing" element={<LANDING />} />,
     </Routes>
     </BrowserRouter>
 
